03d38312f849a5f91effd0447bbd19377730d692
ZafarK638 <zafark@dulwich.org.uk>
2024-09-10 13:15:28 +0100

    .

---
+++ programming/Programming Basics/Worksheets/Worksheet 1/Task3.py
@@ -0,0 +1,5 @@
+# car mileage + total number of litres in tank
+car_mileage_last_fillage = float(input("What was the car's mileage the last time it was filled?"))
+car_mileage_current = float(input("What is the car's mileage now?"))
+total_litres = float(input("What is the total number of litres needed to fill the tank?"))
+car_mileage = float(car_mileage_current + car_mileage_last_fillage)
