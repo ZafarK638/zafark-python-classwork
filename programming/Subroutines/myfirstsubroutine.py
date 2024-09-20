7ff96ccaa298d2e2ce4e8658ec2f5809f8b4662e
ZafarK638 <zafark@dulwich.org.uk>
2024-09-20 09:45:24 +0100

    .

---
+++ programming/Subroutines/myfirstsubroutine.py
@@ -0,0 +1,26 @@
+def multiply(a :int,b :int):
+    print('The product is:', a*b)
+#endprocedure
+
+#inputting values to multiply
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+#calling the function
+multiply(a,b)
+
+
+
+
+def proc1(x :int,y :int) -> None:
+    x=x-2
+    y=0
+    print(x,y)
+#endprocedure
+
+m=8
+n=10
+proc1(m,n)
+print(m,n)
+
+
