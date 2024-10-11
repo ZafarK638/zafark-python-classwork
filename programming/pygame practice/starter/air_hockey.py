1539c086bbe33f813bf9c4e32bde71967c890140
ZafarK638 <zafark@dulwich.org.uk>
2024-10-11 15:32:53 +0100

    Update air_hockey.py

--- programming/pygame practice/starter/air_hockey.py
+++ programming/pygame practice/starter/air_hockey.py
@@ -28,6 +28,16 @@ velocity_x = 1
 velocity_y = 1
 square_width = 50
 square_height = 50
+p1_x = 50
+p1_y = height/2
+
+p2_x = width - p1_x
+p2_y = height/2
+
+move_up_p1 = False
+move_down_p1 = False
+move_up_p2 = False
+move_down_p2 = False
 
 # -------- Main Program Loop -----------
 while not gameOver:
@@ -35,29 +45,90 @@ while not gameOver:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameOver = True
+        
+        if event.type == pygame.KEYDOWN:
+            # --- Player 1 controls
+            if event.key == pygame.K_w:
+                move_up_p1 = True
+            if event.key == pygame.K_s:
+                move_down_p1 = True
+            # --- Player 2 controls
+            if event.key == pygame.K_UP:
+                move_up_p2 = True
+            if event.key == pygame.K_DOWN:
+                move_down_p2 = True
+
+        if event.type == pygame.KEYUP:
+            # --- Player 1 controls
+            if event.key == pygame.K_w:
+                move_up_p1 = False
+            if event.key == pygame.K_s:
+                move_down_p1 = False
+            # --- Player 2 controls
+            if event.key == pygame.K_UP:
+                move_up_p2 = False
+            if event.key == pygame.K_DOWN:
+                move_down_p2 = False
 
     # --- Game logic should go here
 
     if x_pos > (width-square_width):
-        x_pos += -2
+        x_pos += -1
         velocity_x *= -1
     elif x_pos < 0: 
-        x_pos += 2
+        x_pos += 1
         velocity_x *= -1
     else: 
-        x_pos += 2 * (int(velocity_x))
+        x_pos += 1 * (int(velocity_x))
 
 
 
     if y_pos > (height-square_height):
-        y_pos += -2
+        y_pos += -1
         velocity_y *= -1
     elif y_pos < 0:
-        y_pos += 2
+        y_pos += 1
         velocity_y *= -1
     else: 
-        y_pos += 2 * (int(velocity_y))
+        y_pos += 1 * (int(velocity_y))
 
+
+
+    # --- Player 1 code
+    if move_up_p1:
+        if p1_y >= height:
+            p1_y += -1
+        elif p1_y <= 0:
+            p1_y += 1
+        else:
+            p1_y += -1
+
+
+    if move_down_p1:
+        if p1_y >= height:
+            p1_y += -1
+        elif p1_y <= 0:
+            p1_y += 1
+        else:
+            p1_y += 1
+
+    # --- Player 2 controls
+    if move_up_p2:
+        if p2_y >= height:
+            p2_y += -1
+        elif p2_y <= 0:
+            p2_y += 1
+        else:
+            p2_y += -1
+
+
+    if move_down_p2:
+        if p2_y >= height:
+            p2_y += -1
+        elif p2_y <= 0:
+            p2_y += 1
+        else:
+            p2_y += 1
 
     # --- Screen-clearing code goes here
 
@@ -66,19 +137,25 @@ while not gameOver:
 
     # If you want a background image, replace this clear with blit'ing the
     # background image.
-    screen.fill(GREEN)
+    screen.fill(WHITE)
 
     # --- Drawing code should go here
 
+    # --- The bouncy one
     pygame.draw.rect(screen, RED, [x_pos,y_pos,square_width,square_height])
 
+    # --- Player one 
+    pygame.draw.rect(screen, BLACK, [p1_x, p1_y, 20, 100])
+
+    # --- Player two
+    pygame.draw.rect(screen, BLACK, [p2_x, p2_y, 20, 100])
 
 
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
 
     # --- Limit to 60 frames per second
-    clock.tick(60)
+    clock.tick(300)
 
 # Close the window and quit.
 pygame.quit()
