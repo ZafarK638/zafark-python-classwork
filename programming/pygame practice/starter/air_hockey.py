414406c0c2a287e1bcc20389b0fa6c17c769c20e
ZafarK638 <zafark@dulwich.org.uk>
2024-10-12 15:58:58 +0100

    Update air_hockey.py

--- programming/pygame practice/starter/air_hockey.py
+++ programming/pygame practice/starter/air_hockey.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Define some colors
 BLACK = (0, 0, 0)
@@ -10,8 +11,8 @@ BLUE = (0, 0, 255)
 pygame.init()
 
 # Set the width and height of the screen [width, height]
-width = 1000
-height = 700
+width = 1550
+height = 800
 size = (width, height)
 screen = pygame.display.set_mode(size)
 
@@ -26,8 +27,13 @@ x_pos = 0
 y_pos = 0
 velocity_x = 1
 velocity_y = 1
+speed = 1
+
 square_width = 15
 square_height = 15
+
+x_pos = (width - square_width) // 2  
+y_pos = (height - square_height) // 2  
 
 p1_x = 50
 p1_y = height/2
@@ -75,26 +81,51 @@ while not gameOver:
                 move_down_p2 = False
 
     # --- Game logic should go here
+    multiplier = random.randint(1,3)
+    multiplier_s = random.randint(1,3)
 
-    if x_pos > (width-square_width):
-        x_pos += -1
+    x_pos += speed*velocity_x*multiplier_s
+    y_pos += speed*velocity_y*multiplier_s
+
+    # Wall collisions (left and right)
+    if x_pos <= 0 or x_pos >= (width - square_width):
+        # Reset the ball to the center
+        x_pos = (width - square_width) // 2
+        y_pos = (height - square_height) // 2
         velocity_x *= -1
-    elif x_pos < 0: 
-        x_pos += 1
-        velocity_x *= -1
-    else: 
-        x_pos += 1 * (int(velocity_x))
+        multiplier = 1
+        multiplier_s = 1
+        
+
+    # Wall collisions (top and bottom)
+    if y_pos <= 0 or y_pos >= (height - square_height):
+        velocity_y *= -1  # Reverse vertical direction
+
+        if multiplier_s != 1:
+            multiplier_s += -1
+
+    # Paddle collisions
 
 
+    # Check collision with Player 1's paddle
+    if p1_x <= x_pos <= (p1_x + paddle_width):
+        if p1_y <= y_pos <= (p1_y + paddle_height):
+            velocity_x *= -1  # Reverse horizontal direction
 
-    if y_pos > (height-square_height):
-        y_pos += -1
-        velocity_y *= -1
-    elif y_pos < 0:
-        y_pos += 1
-        velocity_y *= -1
-    else: 
-        y_pos += 1 * (int(velocity_y))
+            # Calculate the offset from the center of the paddle
+            offset = (y_pos - (p1_y + paddle_height / 2)) / (paddle_height / 2)
+            # Adjust vertical velocity based on the offset
+            velocity_y = offset * multiplier
+
+    # Check collision with Player 2's paddle
+    if (p2_x - square_width) <= x_pos <= p2_x:
+        if p2_y <= y_pos <= (p2_y + paddle_height):
+            velocity_x *= -1  # Reverse horizontal direction
+            
+            # Calculate the offset from the center of the paddle
+            offset = (y_pos - (p2_y + paddle_height / 2)) / (paddle_height / 2)
+            # Adjust vertical velocity based on the offset
+            velocity_y = offset * multiplier
 
 
 
@@ -102,7 +133,7 @@ while not gameOver:
     if move_up_p1:
         if p1_y >= (height-paddle_height):
             p1_y += -3
-        elif p1_y <= paddle_height:
+        elif p1_y <= 0:
             p1_y += 3
         else:
             p1_y += -3
@@ -111,14 +142,14 @@ while not gameOver:
     if move_down_p1:
         if p1_y >= (height-paddle_height):
             p1_y += -3
-        elif p1_y <= paddle_height:
+        elif p1_y <= 0:
             p1_y += 3
         else:
             p1_y += 3
 
     # --- Player 2 controls
     if move_up_p2:
-        if p2_y >= height:
+        if p2_y >= (height-paddle_height):
             p2_y += -3
         elif p2_y <= 0:
             p2_y += 3
@@ -127,7 +158,7 @@ while not gameOver:
 
 
     if move_down_p2:
-        if p2_y >= height:
+        if p2_y >= (height-paddle_height):
             p2_y += -3
         elif p2_y <= 0:
             p2_y += 3
@@ -153,6 +184,7 @@ while not gameOver:
 
     # --- Player two
     pygame.draw.rect(screen, BLACK, [p2_x, p2_y, paddle_width, paddle_height])
+
 
 
     # --- Go ahead and update the screen with what we've drawn.
