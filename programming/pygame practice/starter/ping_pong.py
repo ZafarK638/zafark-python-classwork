2b5114c257007ed0a79eeb85cfe42b595564f498
ZafarK638 <zafark@dulwich.org.uk>
2024-10-14 10:15:29 +0100

    Update ping_pong.py

--- programming/pygame practice/starter/ping_pong.py
+++ programming/pygame practice/starter/ping_pong.py
@@ -49,6 +49,12 @@ move_down_p1 = False
 move_up_p2 = False
 move_down_p2 = False
 
+increase_speed = False
+decrease_speed = False
+
+score_red = 0
+score_blue = 0
+
 # -------- Main Program Loop -----------
 while not gameOver:
     # --- Main event loop
@@ -67,6 +73,12 @@ while not gameOver:
                 move_up_p2 = True
             if event.key == pygame.K_DOWN:
                 move_down_p2 = True
+            if event.key == pygame.K_m:
+                # speed += 1
+                increase_speed = True
+            if event.key == pygame.K_n:
+                # speed += -1
+                decrease_speed = True
             
             # --- Fullscreen
             if event.key == pygame.K_SPACE:
@@ -85,6 +97,12 @@ while not gameOver:
                 move_up_p2 = False
             if event.key == pygame.K_DOWN:
                 move_down_p2 = False
+            if event.key == pygame.K_m:
+                # speed += 1
+                increase_speed = False
+            if event.key == pygame.K_n:
+                # speed += -1
+                decrease_speed = False
 
     # --- Game logic should go here
     x_pos += speed*velocity_x
@@ -92,15 +110,16 @@ while not gameOver:
 
     # Wall collisions (left and right)
     if x_pos <= 0 or x_pos >= (width - square_width):
-        # Reset the ball to the center
-        x_pos = (width - square_width) // 2
-        y_pos = (height - square_height) // 2
-        multiplier = random.randint(1,5)
-        velocity_x = random.randint(-1,1)
-        if velocity_x == 0:
-            velocity_x += 1
-
-
+        velocity_x *= -1  
+        if x_pos <= 0:
+            score_red += 1
+            scoreR = "Player 2:",score_red
+            print (scoreR)
+        else: 
+            score_blue += 1
+            scoreB = "Player 3:",score_blue
+            print (scoreB)
+        #end if statement'
 
     # Wall collisions (top and bottom)
     if y_pos <= 0 or y_pos >= (height - square_height):
@@ -128,38 +147,50 @@ while not gameOver:
     # --- Player 1 code
     if move_up_p1:
         if p1_y >= (height-paddle_height):
-            p1_y += -3
+            p1_y += -2
         elif p1_y <= 0:
-            p1_y += 3
+            p1_y += 2
         else:
-            p1_y += -3
+            p1_y += -2
 
 
     if move_down_p1:
         if p1_y >= (height-paddle_height):
-            p1_y += -3
+            p1_y += -2
         elif p1_y <= 0:
-            p1_y += 3
+            p1_y += 2
         else:
-            p1_y += 3
+            p1_y += 2
 
     # --- Player 2 controls
     if move_up_p2:
         if p2_y >= (height-paddle_height):
-            p2_y += -3
+            p2_y += -2
         elif p2_y <= 0:
-            p2_y += 3
+            p2_y += 2
         else:
-            p2_y += -3
+            p2_y += -2
 
 
     if move_down_p2:
         if p2_y >= (height-paddle_height):
-            p2_y += -3
+            p2_y += -2
         elif p2_y <= 0:
-            p2_y += 3
+            p2_y += 2
         else:
-            p2_y += 3
+            p2_y += 2
+
+
+    if increase_speed:
+        speed += 0.1
+    
+    if decrease_speed:
+        speed += -0.1
+        if speed <= 0:
+            speed = 1
+
+
+
 
     # --- Screen-clearing code goes here
 
@@ -173,13 +204,13 @@ while not gameOver:
     # --- Drawing code should go here
 
     # --- The bouncy one
-    pygame.draw.rect(screen, RED, [x_pos,y_pos,square_width,square_height])
+    pygame.draw.rect(screen, BLACK, [x_pos,y_pos,square_width,square_height])
 
     # --- Player one 
-    pygame.draw.rect(screen, BLACK, [p1_x, p1_y, paddle_width, paddle_height])
+    pygame.draw.rect(screen, RED, [p1_x, p1_y, paddle_width, paddle_height])
 
     # --- Player two
-    pygame.draw.rect(screen, BLACK, [p2_x, p2_y, paddle_width, paddle_height])
+    pygame.draw.rect(screen, BLUE, [p2_x, p2_y, paddle_width, paddle_height])
 
 
 
@@ -187,7 +218,7 @@ while not gameOver:
     pygame.display.flip()
 
     # --- Limit to 60 frames per second
-    clock.tick(150)
+    clock.tick(300)
 
 # Close the window and quit.
 pygame.quit()
