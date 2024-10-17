9fb583b42106108db18ece045a4e4d18b7cf371e
ZafarK638 <zafark@dulwich.org.uk>
2024-10-17 18:37:48 +0100

    Update stack.py

--- extra_work/stack.py
+++ extra_work/stack.py
@@ -1,16 +1,20 @@
 myString = str(input ("Please enter a word or phrase to be tested: "))
 list1 = list(myString) 	# --- convert myString to a list of characters 
 numChars = len(list1)
-Stack = list()
-s = Stack()
-for i in range (0,numChars-1):
-	s.push(list1[i])
+s = list()
+palindrome = False
+for i in range (numChars):
+	s.append(list1[i])
 	# --- next i
 # --- end for loop
 list2 = list()
-while s.isEmpty == False:
+while len(s) == 0:
 	list2.append(s.pop())
 # --- end while loop
 if list1 == list2:
 	palindrome = True
 # --- End if statement
+if palindrome:
+	print("This is a palindrome. ")
+else:
+	print("This is not a palindrome. ")
