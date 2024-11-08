a1bafbec9c3f5814c0cd3852f091b2f398311e44
ZafarK638 <zafark@dulwich.org.uk>
2024-11-08 15:24:12 +0000

    Update dog.py

--- programming/OOP/dog.py
+++ programming/OOP/dog.py
@@ -1,2 +1,8 @@
-class Dog:
-    
+class Cat:
+    def __init__(self,myName, myColour):
+        self.name = myName
+        self.colour = myColour
+    #end of procedure
+#end of class
+
+my_cat = Cat("Puss in Boots","Orange")
