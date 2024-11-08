1a32b455a88453dc257b05f920952fc8fb35e36e
ZafarK638 <zafark@dulwich.org.uk>
2024-11-08 15:45:52 +0000

    Update cat.py

--- programming/OOP/cat.py
+++ programming/OOP/cat.py
@@ -9,10 +9,20 @@ class Cat:
             print("Meow!")
         # next _
     #end of method
+
+    def change_colour(self,diff_col):
+        self.colour = diff_col
+    #end of method
+
+    def change_name(self,diff_name):
+        self.name = diff_name
+    #end of method
 #end of class
 
 my_cat = Cat("Antonio Banderas","Orange")
 my_cat.meow(2)
 print(my_cat.name)
 my_cat.meow(3)
-print("Program over")
+my_cat.change_colour(str(input()))
+my_cat.change_name(str(input()))
+print(my_cat.colour,my_cat.name)
