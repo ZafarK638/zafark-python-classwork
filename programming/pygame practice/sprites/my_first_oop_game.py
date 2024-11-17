317c29f96a97d63895feffe931a1276045604315
ZafarK638 <zafark@dulwich.org.uk>
2024-11-17 10:30:32 +0000

    Update my_first_oop_game.py

--- programming/pygame practice/sprites/my_first_oop_game.py
+++ programming/pygame practice/sprites/my_first_oop_game.py
@@ -28,17 +28,54 @@ class Block(pygame.sprite.Sprite):
         self.image = pygame.Surface([width, height])
         self.image.fill(colour)
         self.rect = self.image.get_rect()
+
+        self.left_boundary = 0
+        self.right_boundary = 0
+        self.top_boundary = 0
+        self.bottom_boundary = 0
+
+        self.change_x = 0
+        self.change_y = 0
     #end method
 
+    def reset_pos(self):
+        """ Reset position to the top of the screen, at a random x location.
+        Called by update() or the main program loop if there is a collision.
+        """
+        self.rect.y = random.randrange(-300, -20)
+        self.rect.x = random.randrange(0, width)
+    
     def update(self):
         """ Called each frame. """
-        # Move block down one pixel
-        self.rect.y += 1
+        self.rect.x += self.change_x
+        self.rect.y += self.change_y
+ 
+        if self.rect.right >= self.right_boundary or self.rect.left <= self.left_boundary:
+            self.change_x *= -1
+ 
+        if self.rect.bottom >= self.bottom_boundary or self.rect.top <= self.top_boundary:
+            self.change_y *= -1
     #end method
 
+class Player(Block):
+    """ The player class derives from Block, but overrides the 'update'
+    functionality with new a movement function that will move the block
+    with the mouse. """
+    def update(self):
+        # Get the current mouse position. This returns the position
+        # as a list of two numbers.
+        pos = pygame.mouse.get_pos()
+ 
+        # Fetch the x and y out of the list,
+        # just like we'd fetch letters out of a string.
+        # Set the player object to the mouse location
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+
 # Set the width and height of the screen [width, height]
-width = 700
-height = 500
+width = 1000
+height = 700
 size = (width,height)
 screen = pygame.display.set_mode(size)
 
@@ -57,13 +94,20 @@ for i in range(50):
     # Set a random location for the block
     block.rect.x = random.randrange(width)
     block.rect.y = random.randrange(height)
+
+    block.change_x = random.randrange(-3, 4)
+    block.change_y = random.randrange(-3, 4)
+    block.left_boundary = 0
+    block.top_boundary = 0
+    block.right_boundary = width
+    block.bottom_boundary = height
  
     # Add the block to the list of objects
     block_list.add(block)
     all_sprites_list.add(block)
     	
 # Create a RED player block
-player = Block(RED, 20, 15)
+player = Player(RED, 20, 15)
 all_sprites_list.add(player)
 
 # Used to manage how fast the screen updates
@@ -77,6 +121,20 @@ while not gameOver:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameOver = True
+
+    # Call the update() method for all blocks in the block_list
+    block_list.update()
+
+    # See if the player block has collided with anything.
+    blocks_hit_list = pygame.sprite.spritecollide(player, block_list, False)
+    
+    # Check the list of collisions.
+    for block in blocks_hit_list:
+        score += 1
+        print(score)
+    
+        # Reset block to the top of the screen to fall again.
+        block.reset_pos()
 
     # --- Game logic should go here
 
