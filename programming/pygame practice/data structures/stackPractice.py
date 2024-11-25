f4d9bc0c7ec96c5579b2e12e45e6a415c0d7b491
ZafarK638 <zafark@dulwich.org.uk>
2024-11-25 10:24:34 +0000

    Update stackPractice.py

--- programming/pygame practice/data structures/stackPractice.py
+++ programming/pygame practice/data structures/stackPractice.py
@@ -5,23 +5,37 @@ class myStrStack:
         self.data = ["" for _ in range(size)]
     #end constructor
 
-    def popStack (self):
-        pass
-    #end method
-
-    def pushStack (self):
-        pass
-    #end method
-
-    def peek (self):
-        pass
-    #end method
-
     def isEmpty (self):
-        pass
+        return self.sp == -1
     #end method
 
     def isFull (self):
-        pass
+        return self.sp + 1 == self.max
+
+    #end method
+
+    def pushStack (self,item):
+        if not self.isFull:
+            self.sp += 1
+            self.data[self.sp] = item
+        else:
+            print("Stack Full")
+    #end method
+
+    def popStack (self):
+        if not self.isEmpty:
+            poppedValue = self.data[self.sp]
+            self.data[self.sp] = ""
+            self.sp += -1
+            return poppedValue
+        else:
+            print("Stack Empty")
+    #end method
+
+    def peekStack (self):
+        if not self.isEmpty:
+            return self.data[self.sp]
+        else:
+            print("Stack Empty")
     #end method
 #end class
