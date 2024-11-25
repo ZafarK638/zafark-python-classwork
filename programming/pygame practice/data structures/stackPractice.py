7e7523e23c1eb939a395166d9935e74683c065f9
ZafarK638 <zafark@dulwich.org.uk>
2024-11-25 10:06:31 +0000

    Create stackPractice.py

---
+++ programming/pygame practice/data structures/stackPractice.py
@@ -0,0 +1,25 @@
+class myStack:
+    def __init__(self,) -> None:
+        pass
+    #end constructor
+
+    def popStack (self):
+        pass
+    #end method
+
+    def pushStack (self):
+        pass
+    #end method
+
+    def peek (self):
+        pass
+    #end method
+
+    def isEmpty (self):
+        pass
+    #end method
+
+    def isFull (self):
+        pass
+    #end method
+#end class
