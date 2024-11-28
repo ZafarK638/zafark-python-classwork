86374fe6a9f0a571feaa96c1a3cc3e223950a533
ZafarK638 <zafark@dulwich.org.uk>
2024-11-28 10:34:25 +0000

    Update buildings.py.py

--- programming/pygame practice/sprites/buildings.py.py
+++ programming/pygame practice/sprites/buildings.py.py
@@ -4,3 +4,17 @@ class Building:
         self.__width = pWidth #private
         self.__floors = pFloors #private
     #end constructor (new)
+    
+    def getNumberFloors(self):
+        return self.__floors
+    #end method
+
+    def setNumberFloors(self,newFloor):
+        if newFloor >= 1:
+            self.__floors = newFloor
+            return True
+        else:
+            return False
+        #end if-else statement
+    #end method
+#end class
