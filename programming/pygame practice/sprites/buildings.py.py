7bfa91a088d0fe30870f1eea3e889c9bcd717f98
ZafarK638 <zafark@dulwich.org.uk>
2024-11-28 10:29:52 +0000

    Create buildings.py.py

---
+++ programming/pygame practice/sprites/buildings.py.py
@@ -0,0 +1,6 @@
+class Building:
+    def __init__(self,pHeight,pWidth,pFloors) -> None:
+        self.__height = pHeight #private
+        self.__width = pWidth #private
+        self.__floors = pFloors #private
+    #end constructor (new)
