eabce25d5c4f07d404446949e927eba6fec91b11
ZafarK638 <zafark@dulwich.org.uk>
2024-12-03 13:16:18 +0000

    Update my_first_oop_game.py

--- programming/pygame practice/sprites/my_first_oop_game.py
+++ programming/pygame practice/sprites/my_first_oop_game.py
@@ -21,8 +21,23 @@ class Bullet(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
  
     def update(self):
+        mpos = pygame.mouse.get_pos()
+
         #move the bullet
-        self.rect.y += -10
+        # self.rect.x = mpos[0]
+        if self.rect.x > mpos[0]:
+            self.rect.x -= random.randint(bulletSpeedx-2,bulletSpeedx+2)
+        elif self.rect.x < mpos[0]:
+            self.rect.x += random.randint(bulletSpeedx-2,bulletSpeedx+2)
+        elif self.rect.x == mpos[0]:
+            self.rect.x += 0
+
+        if self.rect.y > mpos[1]:
+            self.rect.y -= random.randint(bulletSpeedy-2,bulletSpeedy+2)
+        elif self.rect.y < mpos[1]:
+            self.rect.y += random.randint(bulletSpeedy-2,bulletSpeedy+2)
+        elif self.rect.y == mpos[1]:
+            self.rect.y += 0
 
 class Block(pygame.sprite.Sprite):
     def __init__(self,colour,width,height) -> None:
@@ -60,12 +75,36 @@ class Player(Block):
 
     def update(self):
         # Get the current mouse position. 
-        pos = width/2,height,2
+        pos = width/2,height/2
  
         # Fetch the x and y out of the list,
         # Set the player object to the mouse location
         self.rect.x = pos[0]
-        self.rect.y = height-50
+        self.rect.y = pos[1]
+        
+        if move_right:
+            if self.rect.x < width-pWidth: 
+                self.rect.x += playerSpeed
+            else:
+                self.rect.x = width-pWidth
+
+        if move_left:
+            if self.rect.x > 0:
+                self.rect.x -= playerSpeed
+            else: 
+                self.rect.x = 0
+
+        if move_down:
+            if self.rect.y < height-pHeight: 
+                self.rect.y += playerSpeed
+            else:
+                self.rect.y = height-pHeight
+
+        if move_up:
+            if self.rect.y > 0:
+                self.rect.y -= playerSpeed
+            else: 
+                self.rect.y = 0
 
 
 # Set the width and height of the screen [width, height]
@@ -76,10 +115,17 @@ screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("OOP practise")
 
-pos = width/2
+pos = width/2,height,2
 playerSpeed = 10
+bulletSpeedx = 5
+bulletSpeedy = 5
+pWidth = 20
+pHeight = 15
 move_right = False
 move_left = False
+move_up = False
+move_down = False
+fire_bullet = False
 
 # Loop until the user clicks the close button.
 gameOver = False
@@ -109,7 +155,7 @@ for i in range(50):
     all_sprites_list.add(block)
     	
 # Create a RED player block
-player = Player(RED, 20, 15)
+player = Player(RED, pWidth, pHeight)
 all_sprites_list.add(player)
 
 # Used to manage how fast the screen updates
@@ -126,13 +172,7 @@ while not gameOver:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                #fire a bullet when button pressed
-                bullet = Bullet(BLACK,5,5)
-                #set bullet location
-                bullet.rect.x = player.rect.x
-                bullet.rect.y = player.rect.y
-                all_sprites_list.add(bullet)
-                bullet_list.add(bullet)
+                fire_bullet = True
             if event.key == pygame.K_RIGHT:
                 move_right = True
             if event.key == pygame.K_LEFT:
@@ -143,6 +183,13 @@ while not gameOver:
                 move_right = False
             if event.key == pygame.K_LEFT:
                 move_left = False
+            if event.key == pygame.K_SPACE:
+                fire_bullet = False
+            if event.key == pygame.K_UP:
+                move_right = False
+            if event.key == pygame.K_DOWN:
+                move_left = False
+
 
     # Calculate mechanics for each bullet
     for bullet in bullet_list:
@@ -169,11 +216,15 @@ while not gameOver:
 
     # --- Game logic should go here
 
-    if move_right:
-        pos += playerSpeed
 
-    if move_left:
-        pos -= playerSpeed
+    if fire_bullet:
+        #fire a bullet when button pressed
+        bullet = Bullet(BLACK,5,5)
+        #set bullet location
+        bullet.rect.x = player.rect.x
+        bullet.rect.y = player.rect.y
+        all_sprites_list.add(bullet)
+        bullet_list.add(bullet)
 
     # --- Screen-clearing code goes here
 
@@ -183,8 +234,8 @@ while not gameOver:
     
     # Fetch the x and y out of the list,
     # Set the player object to the mouse location
-    player.rect.x = pos
-    player.rect.y = height-50
+    player.rect.x = pos[0]
+    player.rect.y = pos[1]
 
     blocks_hit_list = pygame.sprite.spritecollide(player, block_list, True)
     for block in blocks_hit_list:
