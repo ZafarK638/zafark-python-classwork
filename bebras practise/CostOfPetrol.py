a86da6a6a94fa21d4bdb56fc144c7ad553be7bfb
ZafarK638 <zafark@dulwich.org.uk>
2025-02-16 17:18:56 +0000

    bebras challenge

--- bebras practise/CostOfPetrol.py
+++ bebras practise/CostOfPetrol.py
@@ -1,11 +1,4 @@
-petrol = input().split()
-while len(petrol) != 12:
-    petrol = input().split()
-for i in range (len(petrol)):
-    if int(petrol[i]) > 200 or int(petrol[i]) < 0 or int(petrol[i]) % 1 != 0:
-        petrol = input().split()
-tot_petrol = float(0.0)
-for i in range(len(petrol)):
-    tot_petrol += (float(petrol[i])*0.0024)
-
-print(int(tot_petrol))
+months = list(map(int, input().split()))
+fuel = sum(months)
+co_two = 0.0024 * fuel
+print(int(co_two))
