ad58fc8d9cdd450c4acd9176065df3508157224b
ZafarK638 <zafark@dulwich.org.uk>
2025-02-16 21:58:38 +0000

    Update chocolate.py

--- bebras practise/chocolate.py
+++ bebras practise/chocolate.py
@@ -1,15 +1,12 @@
 x = input()
-packets = [int(i) for i in x.split()]
-
+chocolates = [int(i) for i in x.split()]
 tot = 0
-packets.sort()
-
+chocolates.sort()
 me = True
-for i in packets:
+for i in chocolates:
     if me:
         tot = tot + i
         me = False
     else:
         me = True
-
 print(tot)
