81d737c85e205abdd3926887b4bfbe2bbb8d8ddb
ZafarK638 <zafark@dulwich.org.uk>
2025-05-02 21:19:51 +0100

    Create quicksort.py

---
+++ programming/sorting algorithms/quicksort.py
@@ -0,0 +1,7 @@
+#Created by Sir Charles Anthony Richardson Hoare (Tony Hoare)
+#Studied classics as Oxford University
+#Quicksort is NOT faster than merge sort - Same speed (depending on the situation it's being applied to)
+#Doesn't use up any extra memory
+
+# Take the first value  as the pivot
+
